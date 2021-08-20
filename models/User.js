26da882c7d7162d4be18a5af8bbaedab63630e3d
Mostafa <mostafa.ahmed.elewa@gmail.com>
2021-08-20 19:49:54 +0200

    build user schema

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const { Mongoose } = require("mongoose");
+
+const userSchema = Mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+const User = Mongoose.model("USer", userSchema);
+export default User;
